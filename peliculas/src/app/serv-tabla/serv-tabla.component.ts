137e88986593dd8c4bb16f90293baeeaddc4a6f0
manana <manana>
2016-11-14 13:55:32 +0100

    act!!

--- peliculas/src/app/serv-tabla/serv-tabla.component.ts
+++ peliculas/src/app/serv-tabla/serv-tabla.component.ts
@@ -10,15 +10,24 @@ import { ServTablaService } from '../serv-tabla.service';
 export class ServTablaComponent implements OnInit {
   
   listaPeliculas: any[];
+  queCabecera: string;
+
   constructor(private servTabla: ServTablaService) { 
     this.listaPeliculas = this.servTabla.listaPeliculas;
   }
 
-  filterSort(){
-    //TODO: Ordenar filas al haccer click en las cabeceras
-    console.log("Funciona");
-    console.log(this.listaPeliculas[0].titulo);
-    for(let i=0; i<this.listaPeliculas.length; i++){
+  sortTable(cabecera: string){
+    //TODO: Ordenar filas al hacer click en las cabeceras
+    //Toda la informacion en internet, se puede sacar, VAMOS!
+    this.queCabecera = cabecera;
+        
+    if(cabecera=='titulo'){
+      
+    }
+    if(cabecera=='autor'){
+      
+    }
+    if(cabecera=='anio'){
       
     }
   }
