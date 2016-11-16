a06b645513890b0a839799fc9605f7b8abb5abb1
manana <manana>
2016-11-16 12:39:10 +0100

    act

--- peliculas/src/app/serv-tabla.service.ts
+++ peliculas/src/app/serv-tabla.service.ts
@@ -9,7 +9,7 @@ export class ServTablaService {
       {id:1, titulo:"Titanic", autor:"Y yo que se", anio:"2016"},
       {id:2, titulo:"Fas and Furious", autor:"Ni idea", anio:"2018"},
       {id:3, titulo:"REC", autor:"A saber", anio:"2012"},
-      {id:4, titulo:"American History X", autor:"Pues no lo se", anio:"1925"},
+      {id:4, titulo:"American History X", autor:"Pues no lo se", anio:"2025"},
       {id:5, titulo:"Mision Imposible", autor:"Tampoco lo se", anio:"1993"}
     ];
   }
--- peliculas/src/app/serv-tabla/serv-tabla.component.ts
+++ peliculas/src/app/serv-tabla/serv-tabla.component.ts
@@ -10,14 +10,15 @@ import { ServTablaService } from '../serv-tabla.service';
 export class ServTablaComponent implements OnInit {
   
   listaPeliculas: any[];
-
+  count: number;
   constructor(private servTabla: ServTablaService) { 
     this.listaPeliculas = this.servTabla.misPeliculas;
+    this.count = 0;
   }
 
   sortTable(cabecera: string){
     console.log(cabecera);
-
+    if(this.count%2===0){
     if(cabecera=="titulo"){
       this.listaPeliculas.sort(function (a, b) {
         if (a.titulo > b.titulo) { return 1; }
@@ -25,6 +26,16 @@ export class ServTablaComponent implements OnInit {
         return 0; // a must be equal to b
       });
     }
+    }else{
+    if(cabecera=="titulo"){
+      this.listaPeliculas.sort(function (a, b) {
+        if (a.titulo < b.titulo) { return 1; }
+        if (a.titulo > b.titulo) { return -1; }
+        return 0; // a must be equal to b
+      });
+    }  
+    }
+    this.count ++;
   }
 
   ngOnInit() {
